a6fdac2c6e072f0e15d05f4c6d91dffd8541cc84
김다운 <rlaekdns1126@naver.com>
2026-08-27 12:16:55 +0900

    조건문random,datetime,if~else

---
+++ p0827/p0827_04.py
@@ -0,0 +1,37 @@
+import random
+import datetime  #현재시간을 가져오는 클래스선언
+
+
+#1-6월까지는 상반기
+#7-12월까지는 하반기
+#8월
+#현재월을 datetime함수를 사용해서 검색한 다음
+#상반기,하반기 출력
+
+#날짜함수를 사용해서 월을 변수에 저장을 한 후
+#비교
+#출력
+
+# now=datetime.datetime.now()
+# month=now.month 
+# if month>=7:
+#      print("{}월 :하반기.".format(month))
+# else:
+#      print("상반기")
+
+
+
+
+#현재시간
+# now = datetime.datetime.now()
+# print("전체시간:",now)          #전체시간
+# print("년도:",now.year)        #년도
+# print("월:",now.month)         #월
+# print("일:",now.day)           #일
+# print("시:",now.hour)          #시
+# print("분:",now.minute)        #분
+# print("초:",now.second)        #초
+
+#2026년 08월 27일 11시 12분 10초
+#format함수사용
+# print("{}년{}월{}일{}시{}분{}초".format(now.year,now.month,now.day,now.hour,now.minute,now.second))
---
+++ p0827/p0827_06.py
@@ -0,0 +1,30 @@
+#조건문을 여러개
+score=65
+if score>=90:
+    print("A")
+elif score>=80:
+    print("B")
+elif score>=70:
+    print("C")
+elif score>=60:
+    print("D")
+else:
+    print("E")
+
+
+
+
+
+
+
+
+
+#조건문안에 조건문
+# a= 40
+# if a>50:
+#     if a<100:
+#         print("50보다 크고 , 100보다 작은수")
+#     else:
+#         print("50보다 크고 , 100보다 큰수")
+# else:
+#     print("50보다 작은수")
